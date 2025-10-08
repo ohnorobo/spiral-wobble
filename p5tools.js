729cf2ebda35f0943ac8bacc607efc4db9eafb7f
Sarah Laplante <sar.laplante@gmail.com>
2025-10-08 20:25:44 +0200

    indentation

--- p5tools.js
+++ p5tools.js
@@ -1,18 +1,20 @@
-  function exportCurrentSVG(filename = 'drawing.svg') {
-    let svgElement = document.querySelector('svg');
-    if (svgElement) {
-        let serializer = new XMLSerializer();
-        let source = serializer.serializeToString(svgElement);
-        let blob = new Blob([source], { type: 'image/svg+xml;charset=utf-8' });
-        let url = URL.createObjectURL(blob);
-        let link = document.createElement("a");
-        link.href = url;
-        link.download = filename;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        URL.revokeObjectURL(url);
-    } else {
-        console.error('No SVG element found for export.');
-    }
+function exportCurrentSVG(filename = 'drawing.svg') {
+  let svgElement = document.querySelector('svg');
+  if (svgElement) {
+      let serializer = new XMLSerializer();
+      let source = serializer.serializeToString(svgElement);
+      let blob = new Blob([source], { type: 'image/svg+xml;charset=utf-8' });
+      let url = URL.createObjectURL(blob);
+      let link = document.createElement("a");
+      link.href = url;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+  } else {
+      console.error('No SVG element found for export.');
+  }
 }
+
+
